8cc0f421aa4e4edb68d4dfbfe6a1fe36c77e5da2
DwanW <kunvang@yahoo.com>
2021-02-16 20:07:04 -0700

    adding some fun event listener functions

--- bot.js
+++ bot.js
@@ -1,20 +1,47 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client();
 
+client.on("ready", () => {
+  console.log("bot is ready");
+});
 
-client.on('ready', () => {
-    console.log('bot is ready')
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!");
+  }
+  if (msg.content.includes("griefer")) {
+    msg.reply("you are talking about Mr.Han, right?");
+  }
+});
+
+client.on("guildMemberAdd", (member) => {
+  const channel = member.guild.channels.cache.find(
+    (ch) => ch.name === "general"
+  );
+  if (!channel) return;
+  channel.send(
+    `Welcome to the server, ${member}, the only reason you are here is for the memes`
+  );
+});
+
+client.on("message", (message) => {
+  if (message.content === "how to embed") {
+    const embed = new MessageEmbed()
+      .setTitle("A slick little embed")
+      .setColor(0xff0000)
+      .setDescription("Hello, this is a slick embed!");
+    message.channel.send(embed);
+  }
+});
+
+client.on("messageDelete", (message) => {
+    message.reply(`${message}, is what you deleted.`);
 })
 
-client.on('message', msg => {
-    if (msg.content === "ping"){
-        msg.reply("Pong!")
-    }
-    if (msg.content.includes('griefer')){
-        msg.reply("you are talking about Mr.Han, right?")
-    }
+client.on("typingStart", (channel, user) => {
+    channel.send(`${user.username} is about to reveal some hidden secrets, listen up everyone!`)
 })
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
