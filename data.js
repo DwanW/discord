abcef6f3d5d2690ef9125b609fda4cf633e5f756
DwanW <kunvang@yahoo.com>
2021-02-19 16:43:57 -0700

    fix typo 2

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const GrieferString = [
-  "Divine Griefer, it means an divine identity who disguise as a human to irritate and harass other players.",
+  "Divine Griefer, it means a divine identity who disguises as a human to irritate and harass other players.",
   "Griefer God, he is the most prominent among the griefers.",
-  "Grifer, it means A person who harasses or deliberately provokes other players or members in order to spoil their enjoyment.",
+  "Griefer, it means a person who harasses or deliberately provokes other players in order to spoil their enjoyment.",
 ];
 
 module.exports = { GrieferString };
