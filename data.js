ed9db1b80cae111e2ff589b8652342a38c741b35
DwanW <kunvang@yahoo.com>
2021-02-25 12:04:22 -0700

    adding aditional special string

--- data.js
+++ data.js
@@ -2,6 +2,7 @@ const GrieferString = [
   "Divine Griefer, it means a divine identity who disguises as a human to irritate and harass other players.",
   "Griefer God, he is the most prominent among the griefers.",
   "Griefer, it means a person who harasses or deliberately provokes other players in order to spoil their enjoyment.",
+  "SB, it means what it means, definitely not a problem calling your friend that",
 ];
 
 module.exports = { GrieferString };
