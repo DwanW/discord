16dfdf2bdbe59406d837c94528ab304564f04597
DwanW <kunvang@yahoo.com>
2021-03-27 23:48:33 -0600

    remove audio command

--- bot.js
+++ bot.js
@@ -67,21 +67,21 @@ client.on("message", async (msg) => {
       );
     msg.channel.send(embed);
   }
-  if (msg.content === "!nb") {
-    if (msg.author.discriminator === "0013") {
-      msg.channel.send("?");
-      setTimeout(() => msg.channel.send("我为什么说我自己牛逼"), 2000);
-    } else {
-      if (msg.member.voice.channel) {
-        const connection = await msg.member.voice.channel.join();
-        connection.play("voice-nb.mp3").on("finish", () => {
-          msg.member.voice.channel.leave();
-        });
-      }
-      msg.channel.send("韩师傅牛逼 - Master Han is Awesome");
-    }
-  }
-});
+//   if (msg.content === "!nb") {
+//     if (msg.author.discriminator === "0013") {
+//       msg.channel.send("?");
+//       setTimeout(() => msg.channel.send("我为什么说我自己牛逼"), 2000);
+//     } else {
+//       if (msg.member.voice.channel) {
+//         const connection = await msg.member.voice.channel.join();
+//         connection.play("voice-nb.mp3").on("finish", () => {
+//           msg.member.voice.channel.leave();
+//         });
+//       }
+//       msg.channel.send("韩师傅牛逼 - Master Han is Awesome");
+//     }
+//   }
+// });
 
 client.on("guildMemberAdd", (member) => {
   const channel = member.guild.channels.cache.find(
